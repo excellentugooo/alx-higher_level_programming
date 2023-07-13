ac73195e0fe4e9a1ba68ee27a263c9224ff75cab
excellentugooo <ugooo.okonkwo@gmail.com>
2023-07-13 11:50:07 -0700

    fixing bugs

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,8 +4,8 @@ def magic_calculation(a, b):
 
     if a < b:
         c = add(a, b)
-        for index in range(4, 6):
-            c = add(c, index)
+        for i in range(4, 6):
+            c = add(c, i)
         return (c)
 
     else:
