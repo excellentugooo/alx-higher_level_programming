d1208efa9f4ca152e2b93815bd9f78259c8520d8
excellentugooo <ugooo.okonkwo@gmail.com>
2023-07-17 15:16:56 -0700

    advanced task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,9 +1,11 @@
 #include <Python.h>
+#include <stdio.h>
 
 /**
  * print_python_list_info - Prints basic info about Python lists.
  * @p: A PyObject list.
  */
+
 void print_python_list_info(PyObject *p)
 {
 	int size, allocate, x;
