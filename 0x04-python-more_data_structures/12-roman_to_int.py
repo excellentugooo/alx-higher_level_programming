cb7599332249acdb22ba3bd358c0bc92d3c99c0f
excellentugooo <ugooo.okonkwo@gmail.com>
2023-07-19 12:32:37 -0700

    debug

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python3
-def to_sub(List):
+def to_subtract(num_list):
     subtract = 0
-    MaxList = max(List)
-    for i in List:
-        if MaxList > i:
-            subtract += i
-    return (MaxList - subtract)
+    max_num = max(num_list)
+    for num in num_list:
+        if max_num > num:
+            subtract += num
+    return max_num - subtract
 
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    RomanNum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(RomanNum.keys())
-    number = 0
-    last_rom = 0
-    list_num = [0]
-    for i in roman_string:
-        for r_num in list_keys:
-            if r_num == i:
-                if RomanNum.get(i) <= last_rom:
-                    number += to_sub(list_num)
-                    list_num = [RomanNum.get(i)]
-                else:
-                    list_num.append(RomanNum.get(i))
 
-                last_rom = RomanNum.get(i)
-    number += to_sub(list_num)
-    return (number)
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    numbers = []
+    prev_num = 0
+
+    for char in roman_string:
+        if char in roman_numerals:
+            current_num = roman_numerals[char]
+            if current_num > prev_num:
+                numbers.append(to_subtract(numbers))
+                numbers = [current_num]
+            else:
+                numbers.append(current_num)
+            prev_num = current_num
+
+    numbers.append(to_subtract(numbers))
+    return sum(numbers)
