1445ec8f6c199a29bbb30df6041db97d69be8d7a
excellentugooo <ugooo.okonkwo@gmail.com>
2023-07-19 12:36:00 -0700

    debug

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
-def to_subtract(num_list):
-    subtract = 0
-    max_num = max(num_list)
-    for num in num_list:
-        if max_num > num:
-            subtract += num
-    return max_num - subtract
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
 
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string:
         return 0
 
-    roman_numerals = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
+    if not isinstance(roman_string, str):
+        return 0
 
-    numbers = []
-    prev_num = 0
+    rom_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C':
+                100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_nums.keys())
 
-    for char in roman_string:
-        if char in roman_numerals:
-            current_num = roman_numerals[char]
-            if current_num > prev_num:
-                numbers.append(to_subtract(numbers))
-                numbers = [current_num]
-            else:
-                numbers.append(current_num)
-            prev_num = current_num
+    num = 0
+    last_rom = 0
+    list_num = [0]
 
-    numbers.append(to_subtract(numbers))
-    return sum(numbers)
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_nums.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_nums.get(ch)]
+                else:
+                    list_num.append(rom_nums.get(ch))
+
+                last_rom = rom_nums.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
