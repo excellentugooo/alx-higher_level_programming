384b66df22a90fb9a5f24f917b7d328eec620978
excellentugooo <ugooo.okonkwo@gmail.com>
2023-07-31 09:42:40 -0700

    Debugging

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     try:
         for i in range(x):
-            print(my_list[index], end='')
+            print(my_list[i], end='')
             ret += 1
     except IndexError:
         None
-    print()
+    print('')
     return ret
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     num = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[index]), end='')
+            print("{:d}".format(my_list[i]), end='')
             num += 1
         except (ValueError, TypeError):
             pass
-    print()
+    print('')
     return num
