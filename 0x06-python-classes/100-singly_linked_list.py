ba3428a2a1a70447539bb63d39e09b3d3a6deaef
excellentugooo <ugooo.okonkwo@gmail.com>
2023-08-01 05:06:00 -0700

    corrections

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -63,7 +63,7 @@ class SinglyLinkedList:
             while (tmp.next_node is not None and
                     tmp.next_node.data < value):
                 tmp = tmp.next_node
-            new.next_node = tmp.next_node
+            insert.next_node = tmp.next_node
             tmp.next_node = insert
 
     def __str__(self):
